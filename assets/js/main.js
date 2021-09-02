d0ee1321da8d5bd0d7a17f401e9d2702e61afb59
n-deleforge <61833158+n-deleforge@users.noreply.github.com>
2021-09-02 09:41:18 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,30 @@
-// Start
+// Call the main function
 displayInfo();
 
 // Prevent right click on the picture
 document.getElementById("photo").addEventListener('contextmenu', event => event.preventDefault());
 
-// Functions
+/**
+ * Slowly show every elem classes
+ **/
 
-// Slowly show the differents
 async function displayInfo() {
-    await showAfter(500).then(() => {
-        document.getElementById("header").classList.add("fadeIn");
-        document.getElementById("header").style.visibility = "visible";
-    });
+    const containers = document.querySelectorAll(".elem");
 
-    await showAfter(1500).then(() => {
-        document.getElementById("company").classList.add("fadeIn");
-        document.getElementById("company").style.visibility = "visible";
-    });
-
-    await showAfter(1500).then(() => {
-        document.getElementById("social").classList.add("fadeIn");
-        document.getElementById("social").style.visibility = "visible";
-    });
+    for(let i = 0; i < containers.length; i++) {
+        await showAfter(1000).then(() => {
+            containers[i].classList.add("fadeIn");
+            containers[i].style.visibility = "visible";
+        });
+    }
 }
 
-// Resolve a promise after the timeout is over
+/**
+ * Set a timeout and resolve the promise when it's over
+ **/
+
 function showAfter(timeout) {
     return new Promise(resolve => {
-        setTimeout(() => {
-            resolve();
-        }, timeout);
+        setTimeout(() => { resolve() }, timeout);
     });
 };
