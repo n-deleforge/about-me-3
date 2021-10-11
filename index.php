aa61b3978f3f7ff8da8e6f32ec0bf4fcc8cc8820
n-deleforge <61833158+n-deleforge@users.noreply.github.com>
2021-10-11 18:52:49 +0200

    renaming constant variables

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-    $appVersion = "1.0";
-    $titlePage = "Nicolas Deleforge";
-    $description = "Développeur web, sur cette page, je vous présente mes différents réseaux et mon entreprise de réalisation de site internet et d'applications web.";
-    $mainURL = "https://nicolas-deleforge.fr";
-    $bannerURL = "https://nicolas-deleforge.fr/about-me/assets/image/banner.png";
+    $APP_VERSION = "1.0";
+    $TITLE_PAGE = "Nicolas Deleforge";
+    $DESCRIPTION = "Développeur web, sur cette page, je vous présente mes différents réseaux et mon entreprise de réalisation de site internet et d'applications web.";
+    $MAIN_URL = "https://nicolas-deleforge.fr";
+    $BANNER_URL = "https://nicolas-deleforge.fr/about-me/assets/image/banner.png";
+    $LINKS = [
+        ["https://forgecode.fr", "fas fa-hammer", "ForgeCode"],
+        ["https://github.com/n-deleforge", "fab fa-github", "Github"],
+        ["https://linkedin.com/in/nicolasdeleforge", "fab fa-linkedin", "LinkedIn"]
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -13,23 +18,23 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?php echo $titlePage; ?></title>
-    <meta name="title" content="<?php echo $titlePage; ?>">
-    <meta name="description" content="<?php echo $description; ?>">
+    <title><?php echo $TITLEPAGE; ?></title>
+    <meta name="title" content="<?php echo $TITLEPAGE; ?>">
+    <meta name="description" content="<?php echo $DESCRIPTION; ?>">
 
     <meta property="og:type" content="website">
-    <meta property="og:url" content="<?php echo $mainURL; ?>">
-    <meta property="og:title" content="<?php echo $titlePage; ?>">
-    <meta property="og:description" content="<?php echo $description; ?>">
-    <meta property="og:image" content="<?php echo $bannerURL; ?>">
+    <meta property="og:url" content="<?php echo $MAIN_URL; ?>">
+    <meta property="og:title" content="<?php echo $TITLEPAGE; ?>">
+    <meta property="og:description" content="<?php echo $DESCRIPTION; ?>">
+    <meta property="og:image" content="<?php echo $BANNER_URL; ?>">
 
     <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="<?php echo $mainURL; ?>">
-    <meta property="twitter:title" content="<?php echo $titlePage; ?>">
-    <meta property="twitter:description" content="<?php echo $description; ?>">
-    <meta property="twitter:image" content="<?php echo $bannerURL; ?>">
+    <meta property="twitter:url" content="<?php echo $MAIN_URL; ?>">
+    <meta property="twitter:title" content="<?php echo $TITLEPAGE; ?>">
+    <meta property="twitter:description" content="<?php echo $DESCRIPTION; ?>">
+    <meta property="twitter:image" content="<?php echo $BANNER_URL; ?>">
 
-    <link rel="stylesheet" href="assets/css/main.min.css?v=<?php echo $appVersion; ?>">
+    <link rel="stylesheet" href="assets/css/main.min.css?v=<?php echo $APP_VERSION; ?>">
     <link rel="stylesheet" href="assets/css/fontawesome/all.min.css">
 
     <link rel="apple-touch-icon" sizes="180x180" href="assets/image/favicon/apple-touch-icon.png">
@@ -50,12 +55,13 @@
         </div>
 
         <div id="buttonList">
-            <a class="button elem" target="_blank" rel="noreferrer" href="https://forgecode.fr"><i class="fas fa-hammer"></i> ForgeCode</a>
-            <a class="button elem" target="_blank" rel="noreferrer" href="https://github.com/n-deleforge"><i class="fab fa-github"></i> GitHub</a>
-            <a class="button elem" target="_blank" rel="noreferrer" href="https://linkedin.com/in/nicolasdeleforge"><i class="fab fa-linkedin"></i> LinkedIn</a>
+            <?php 
+                foreach($LINKS as $link)
+                    echo '<a class="button elem" target="_blank" rel="noreferrer" href="' . $link[0] . '"><i class="' . $link[1] . '"></i> ' . $link[2] . '</a>';
+            ?>
         </div>
 
         <script src="https://nicolas-deleforge.fr/libraries/littleJS.min.js"></script>
-        <script src="assets/js/main.min.js?v=<?php echo $appVersion; ?>"></script>
+        <script src="assets/js/main.min.js?v=<?php echo $APP_VERSION; ?>"></script>
 </body>
 </html>
